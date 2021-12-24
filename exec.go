b786646aa9942d9c84c80973da31d53d0ea71451
jojomi <dev.jojomi@yahoo.com>
2021-12-24 01:52:24 +0100

    Add exec helpers

---
+++ exec.go
@@ -0,0 +1,29 @@
+package tplfuncs
+
+import (
+	htmlTemplate "html/template"
+	"os/exec"
+	"strings"
+	textTemplate "text/template"
+)
+
+// ExecHelpers returns a text template FuncMap with functions related to command execution
+func ExecHelpers() textTemplate.FuncMap {
+	return textTemplate.FuncMap{
+		"exec": execFunc,
+	}
+}
+
+// ExecHelpersHTML returns a HTML template FuncMap with functions related to command execution
+func ExecHelpersHTML() htmlTemplate.FuncMap {
+	return htmlTemplate.FuncMap(ExecHelpers())
+}
+
+func execFunc(command string) (string, error) {
+	parts := strings.Fields(command)
+	out, err := exec.Command(parts[0], parts[1:]...).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
---
+++ exec_test.go
@@ -0,0 +1,41 @@
+package tplfuncs
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExec(t *testing.T) {
+	assrt := assert.New(t)
+
+	input := `/bin/echo abc`
+	expected := "abc\n"
+
+	out, err := execFunc(input)
+	assrt.Nil(err)
+	assrt.Equal(expected, out)
+}
+
+func TestExecFail(t *testing.T) {
+	assrt := assert.New(t)
+
+	input := `/bin/invalid_binary_name`
+
+	_, err := execFunc(input)
+	assrt.NotNil(err)
+}
+
+func TestExecTemplate(t *testing.T) {
+	assrt := assert.New(t)
+
+	funcMap := MakeHTMLFuncMap(ExecHelpersHTML())
+	emptyData := struct{}{}
+
+	input := `a {{- exec "/bin/echo test" -}} b`
+	expected := `atest
+b`
+
+	output, err := executeTemplateWithHTMLFuncMap(funcMap, input, emptyData)
+	assrt.Nil(err, "exec function failed")
+	assrt.Equal(expected, output)
+}
