b775fce76cf1f7a0f8b60bd2ef654f1d50f4cf4f
jojomi <jojomi.dev@yahoo.com>
2022-08-27 23:44:09 +0200

    Add output funcs

--- go.mod
+++ go.mod
@@ -3,16 +3,20 @@ module github.com/jojomi/tplfuncs
 go 1.17
 
 require (
+	github.com/cloudfoundry-attic/jibber_jabber v0.0.0-20151120183258-bcc4c8345a21
 	github.com/juju/errors v1.0.0
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/stretchr/testify v1.8.0
+	golang.org/x/text v0.3.7
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jeandeaual/go-locale v0.0.0-20220711133428-7de61946b173 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1-0.20211023094830-115ce09fd6b4 // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ output.go
@@ -0,0 +1,42 @@
+package tplfuncs
+
+import (
+	"github.com/cloudfoundry-attic/jibber_jabber"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	htmlTemplate "html/template"
+	textTemplate "text/template"
+)
+
+// OutputHelpers returns a text template FuncMap with output related functions
+func OutputHelpers() textTemplate.FuncMap {
+	return textTemplate.FuncMap{
+		"printfLocalized": printfLocalized,
+		"printInt": func(value int) (string, error) {
+			return printfLocalized("%d", value)
+		},
+		"printInt64": func(value int64) (string, error) {
+			return printfLocalized("%d", value)
+		},
+	}
+}
+
+// OutputHelpersHTML returns an HTML template FuncMap with output related functions
+func OutputHelpersHTML() htmlTemplate.FuncMap {
+	return htmlTemplate.FuncMap(OutputHelpers())
+}
+
+func printfLocalized(key message.Reference, data ...interface{}) (string, error) {
+	_, err := locale.GetLocales()
+	if err != nil {
+		return "", err
+	}
+	/// fmt.Println(message.MatchLanguage(userLocales...))
+	lang, err := jibber_jabber.DetectLanguage()
+	if err != nil {
+		return "", err
+	}
+	p := message.NewPrinter(language.MustParse(lang))
+	return p.Sprintf(key, data...), nil
+}
