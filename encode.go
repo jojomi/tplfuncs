52bdcb600d882a431e35c66b66afd6b960d76c93
jojomi <dev.jojomi@yahoo.com>
2024-08-17 14:28:42 +0200

    Add escapeChar function

--- encode.go
+++ encode.go
@@ -3,14 +3,16 @@ package tplfuncs
 import (
 	htmlTemplate "html/template"
 	"net/url"
+	"strings"
 	textTemplate "text/template"
 )
 
 // EncodeHelpers returns a text template FuncMap with encode functions
 func EncodeHelpers() textTemplate.FuncMap {
 	return textTemplate.FuncMap{
-		"urlencode": urlencodeFunc,
-		"urldecode": urldecodeFunc,
+		"urlencode":   urlencodeFunc,
+		"urldecode":   urldecodeFunc,
+		"escapeChars": escapeCharsFunc,
 	}
 }
 
@@ -31,4 +33,17 @@ func urldecodeFunc(input string) (string, error) {
 		return "", err
 	}
 	return decoded, nil
+}
+
+// Doc: `escapeChars` returns the input with any of the given chars escaped with the escaping char.
+func escapeCharsFunc(input, escapeChars, escapingChar string) string {
+	result := input
+	for _, char := range input {
+		if strings.ContainsRune(escapeChars, char) {
+			result += escapingChar + string(char)
+		} else {
+			result += string(char)
+		}
+	}
+	return result
 }
